34c322dc6bfa82eb4b0a43f76f3241dbf5580a00
mahikero <federicoparassarmiento@yahoo.com>
2017-01-23 06:50:05 -0500

    up

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KeyUpComponent } from './key-up.component'
 
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    declarations: [KeyUpComponent],
+    bootstrap: [KeyUpComponent]
 })
 
 export class AppModule{}
---
+++ app/key-up.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'key-up',
+    template: `
+    <h1>rico</h1>
+    `
+})
+
+export class KeyUpComponent {
+
+}
